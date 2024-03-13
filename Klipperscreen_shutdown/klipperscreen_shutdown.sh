552b4ebfa73cb05d5dc74280560ec638b896bf85
leadustin <155164500+leadustin@users.noreply.github.com>
2024-03-13 20:08:42 +0100

    Update klipperscreen_shutdown.sh

--- Klipperscreen_shutdown/klipperscreen_shutdown.sh
+++ Klipperscreen_shutdown/klipperscreen_shutdown.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+
+# IP-Adresse des Klipperscreens
+ip="192.168.188.46"
+
+# Benutzername und Passwort
+user="mks"
+password="makerbase"
+
+# Countdown-Funktion definieren
+countdown() {
+    for i in {5..1}; do
+        echo "Herunterfahren des Klipperscreens in: $i"
+        sleep 1
+    done
+    echo "Herunterfahren des Klipperscreens in: 0"
+}
+
+# Countdown ausgeben
+echo "Herunterfahren des Klipperscreens in:"
+countdown
+
+# SSH-Verbindung mit deaktivierter Host-Key-Prüfung herstellen und Klipperscreen herunterfahren
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no $user@$ip "sudo shutdown -h now -n"
+
+# Überprüfen, ob der Klipperscreen heruntergefahren wurde
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no $user@$ip "pgrep klipperscreen > /dev/null"
+if [ $? -eq 0 ]; then
+  echo "Fehler: Klipperscreen wurde nicht heruntergefahren!"
+  exit 1
+fi
+
+echo "Herunterfahren abgeschlossen!"
 
